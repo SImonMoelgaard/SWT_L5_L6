a41a71c7ed56b98fac901d8ecd370bb0e564264d
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-02-25 13:51:24 +0100

    Substitute test lavet

--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using NSubstitute;
 using NUnit.Framework;
 using SWT_L5_6_Interfaces;
 
@@ -8,23 +9,28 @@ namespace NUnitTestProject
     public class Tests
     {
         private Ecs _uut;
+        private IHeater _heater;
+        private ITempSensor _tempsensor;
         [SetUp]
         public void Setup()
         {
-            Ecs_ctor_inject();
+            _heater = Substitute.For<IHeater>();
+            _tempsensor = Substitute.For<ITempSensor>();
+            _uut = new Ecs(25,_tempsensor,_heater);
+            //Ecs_ctor_inject();
         }
 
         [Test]
         public void Ecs_ctor_inject()
         {
-            _uut = new Ecs(5, new FakeTempSensor(5), new FakeHeater(true));
+            //_uut = new Ecs(5, new FakeTempSensor(5), new FakeHeater(true));
             
         }
 
         [Test]
         public void GetThreshold()
         {
-            Assert.That(_uut.GetThreshold(), Is.EqualTo(5));
+            Assert.That(_uut.GetThreshold(), Is.EqualTo(25));
         }
 
 
@@ -32,15 +38,20 @@ namespace NUnitTestProject
         public void Regulate()
         {
 
-          
-            FakeTempSensor fakeTempTest = new FakeTempSensor(1);
-            FakeHeater fakeHeaterTest = new FakeHeater(false);
-            _uut = new Ecs(5, fakeTempTest, fakeHeaterTest);
-            _uut.SetThreshold(2);
+            _tempsensor.GetTemp().Returns(24);
+            _uut.Regulate();
+            _heater.Received(1).TurnOn();
+
+
+
+            //FakeTempSensor fakeTempTest = new FakeTempSensor(1);
+            //FakeHeater fakeHeaterTest = new FakeHeater(false);
+            //_uut = new Ecs(5, fakeTempTest, fakeHeaterTest);
+            //_uut.SetThreshold(2);
             
 
-            _uut.Regulate();
-            Assert.IsTrue(fakeHeaterTest.Answer);
+            //_uut.Regulate();
+            //Assert.IsTrue(fakeHeaterTest.Answer);
         }
 
 
@@ -63,7 +74,7 @@ namespace NUnitTestProject
         [Test]
         public void GetCurTemp()
         {
-            
+            _tempsensor.GetTemp().Returns(5);
             Assert.That(_uut.GetCurTemp(),Is.EqualTo(5));
             
             
@@ -73,8 +84,9 @@ namespace NUnitTestProject
         [Test]
         public void RunSelfTest()
         {
-            _uut.RunSelfTest();
-            Assert.IsTrue(true);
+            _tempsensor.RunSelfTest().Returns(false);
+            _heater.RunSelfTest().Returns(true);
+            Assert.IsFalse(_uut.RunSelfTest());
         }
 
     }
